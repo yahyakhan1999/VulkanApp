5ff0790af657fc99a15a59fd4b54f99ef8dc96df
Yahya Khan <yahya.b.khan@gmail.com>
2025-12-11 15:56:13 -0500

    Framebuffers

--- src/main.cpp
+++ src/main.cpp
@@ -75,6 +75,8 @@ private:
     VkPipelineLayout pipelineLayout;
     VkPipeline graphicsPipeline;
     VkRenderPass renderPass;
+    std::vector<VkFramebuffer> swapChainFramebuffers;
+
     // Init GLFW window
     void initWindow() {
         if (!glfwInit()) {
@@ -101,7 +103,31 @@ private:
         createRenderPass();
         createPipelineLayout();
         createGraphicsPipeline();
+        createFramebuffers();
 
+    }
+
+    void createFramebuffers() {
+        swapChainFramebuffers.resize(swapChainImageViews.size());
+    
+        for (size_t i = 0; i < swapChainImageViews.size(); i++) {
+            VkImageView attachments[] = {
+                swapChainImageViews[i]
+            };
+
+            VkFramebufferCreateInfo framebufferInfo{};
+            framebufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+            framebufferInfo.renderPass = renderPass;
+            framebufferInfo.attachmentCount = 1;
+            framebufferInfo.pAttachments = attachments;
+            framebufferInfo.width = swapChainExtent.width;
+            framebufferInfo.height = swapChainExtent.height;
+            framebufferInfo.layers = 1;
+
+            if (vkCreateFramebuffer(device, &framebufferInfo, nullptr, &swapChainFramebuffers[i]) != VK_SUCCESS) {
+                throw std::runtime_error("failed to create framebuffer!");
+            }
+        }
     }
 
     void createRenderPass() {
@@ -746,6 +772,10 @@ private:
     }
 
     void cleanup() {
+
+          for (auto framebuffer : swapChainFramebuffers) {
+            vkDestroyFramebuffer(device, framebuffer, nullptr);
+        }
             vkDestroyRenderPass(device, renderPass, nullptr);
             vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
             vkDestroyPipeline(device, graphicsPipeline, nullptr);
